891d51599ea86bf9082629dc47791f3df38241b9
Kashish <lunakanu085@gmail.com>
2026-01-30 01:22:19 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # helps you create classes mainly to store data
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 # for taking input files
@@ -48,6 +50,8 @@ if __name__ == "__main__":
     train_data, test_data, raw_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
 
+    ModelTrainer = ModelTrainer()
+    print(ModelTrainer.initiate_model_trainer(train_arr,test_arr))
 
--- src/utlis.py
+++ src/utlis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 import pickle
 import dill
@@ -16,5 +18,20 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    '''
+    This function evaluates multiple machine learning models and returns their performance scores.
+    '''
+    try:
+        model_report = {}
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2_square = r2_score(y_test, y_pred)
+            model_report[name] = r2_square
+        return model_report
     except Exception as e:
         raise CustomException(e, sys)
